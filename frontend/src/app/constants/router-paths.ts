549b913bf868fe1667e301d40ff158f0d2907159
Vladislav Zheromsky <vladislavzheromsky@gmail.com>
2021-03-01 16:09:20 +0300

    [FE] feat: add basic constants and models

---
+++ frontend/src/app/constants/router-paths.ts
@@ -0,0 +1,19 @@
+import { CommonModel } from '../models/common';
+
+export const routerPaths: CommonModel = {
+  home: '',
+  login: 'login',
+  myProfile: 'my-profile',
+  courses: 'courses',
+  groups: 'groups',
+  teachers: 'teachers',
+  students: 'students',
+  discounts: 'discounts',
+  payment: 'payment',
+  analytics: 'analytics',
+  messages: 'messages',
+  schedule: 'schedule',
+  testing: 'testing',
+  game: 'game',
+  connect: 'connect',
+};
---
+++ frontend/src/app/models/navigation-link.ts
@@ -0,0 +1,7 @@
+import { CommonModel } from './common';
+
+export interface NavigationLinkModel {
+  name: string;
+  url: string;
+  icon: string;
+}
