b22096687396a6873e87319d888677a51a064fb3
Vladislav Zheromsky <vladislavzheromsky@gmail.com>
2021-05-02 16:44:12 +0300

    [FE] feat: add shared module

---
+++ frontend/src/app/shared/shared.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { PasswordModule } from 'primeng/password';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { DropdownModule } from 'primeng/dropdown';
+import { KnobModule } from 'primeng/knob';
+import { CalendarModule } from 'primeng/calendar';
+import { MessageModule } from 'primeng/message';
+import { ToastModule } from 'primeng/toast';
+import { TableModule } from 'primeng/table';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    DividerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PasswordModule,
+    InputTextareaModule,
+    DropdownModule,
+    KnobModule,
+    CalendarModule,
+    MessageModule,
+    ToastModule,
+    TableModule,
+  ],
+  exports: [
+    CommonModule,
+    ButtonModule,
+    DividerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PasswordModule,
+    InputTextareaModule,
+    DropdownModule,
+    KnobModule,
+    CalendarModule,
+    MessageModule,
+    ToastModule,
+    TableModule,
+  ],
+})
+export class SharedModule {}
