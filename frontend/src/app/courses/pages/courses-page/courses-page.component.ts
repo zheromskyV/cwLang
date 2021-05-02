ebc892e4118ab6d7a4597c633179d20efc122465
Vladislav Zheromsky <vladislavzheromsky@gmail.com>
2021-05-02 18:52:26 +0300

    [FE] feat: add courses page

--- frontend/src/app/courses/pages/courses-page/courses-page.component.ts
+++ frontend/src/app/courses/pages/courses-page/courses-page.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { dictionary } from 'src/app/constants/dictionary';
+
 @Component({
   selector: 'cwl-courses-page',
   templateUrl: './courses-page.component.html',
   styleUrls: ['./courses-page.component.scss'],
 })
 export class CoursesPageComponent implements OnInit {
+  dictionary = dictionary;
+
   constructor() {}
 
   ngOnInit(): void {}
