b008fbde3a12d1ab4d469325ab6a8f4bc051475b
Vladislav Zheromsky <vladislavzheromsky@gmail.com>
2021-05-10 22:32:53 +0300

    [FE] feat: add schedule component

--- frontend/src/app/schedule/schedule.module.ts
+++ frontend/src/app/schedule/schedule.module.ts
@@ -4,9 +4,10 @@ import { FullCalendarModule } from 'primeng/fullcalendar';
 import { SharedModule } from '../shared/shared.module';
 import { ScheduleRoutingModule } from './schedule-routing.module';
 import { SchedulePageComponent } from './pages/schedule-page/schedule-page.component';
+import { ScheduleCalendarComponent } from './components/schedule-calendar/schedule-calendar.component';
 
 @NgModule({
-  declarations: [SchedulePageComponent],
+  declarations: [SchedulePageComponent, ScheduleCalendarComponent],
   imports: [SharedModule, FullCalendarModule, ScheduleRoutingModule],
 })
 export class ScheduleModule {}
